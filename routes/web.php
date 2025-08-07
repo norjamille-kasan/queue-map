6ff2fa8e00a7c11cea0d37cfca02330fd845005b
Jam <jamclong0328@gmail.com>
2025-08-07 16:20:53 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+
+});
+
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
